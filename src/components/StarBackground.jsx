f40df9a51e5e53553932dfdbc373c255d6a22cac
GayatriPokale <gayatripokale247@gmail.com>
2025-05-30 12:33:03 -0700

    add Navbar

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 export const StarBackground = () => {
   const [stars, setStars] = useState([]);
   const [meteors, setMeteors] = useState([]);
-
+  
   useEffect(() => {
     generateStars();
     generateMeteors();
